293f09d424d1050731273774959833ceb8a8d350
marutiagarwal <marutihereagain@gmail.com>
2015-09-15 10:59:30 +0530

    rearranged code

--- springleaf/scripts/featureProcessing.py
+++ springleaf/scripts/featureProcessing.py
@@ -37,52 +37,6 @@ from sklearn.preprocessing import StandardScaler
 # 4. There are bunch of (NA, -1, "",[]) features to take care of
 
 
-def feature_type(X):
-	nrows, ncols = X.shape
-	count_str = 0
-	count_int = 0
-	count_bool = 0
-	count_float = 0
-	count_timestamp = 0
-	idx_str = []
-	idx_bool = []
-	idx_int = []
-	idx_float = []
-	idx_timestamp = []
-
-	row = 0
-	for x in xrange(0,ncols):
-		# print 'x = ',x,', type = ',type(X[row,x]),', X[row,x] = ',X[row,x]
-		if type(X[row,x]) is float and math.isnan(float(X[row,x])):
-			print 'math.isnan'
-		# while math.isnan(float(X[row,x])):
-		# 	row += 10
-		# 	print 'X[row,x] = ',X[row,x]
-		if(type(X[row,x]) is str):
-			# print 'x = ',x,', type = ',type(X[row,x])
-			# print 'X[row,x] = ',X[row,x]
-			datetimestring = X[row,x]
-			line = datetimestring[-9:]
-			ret = re.match(':\d{2}:\d{2}:\d{2}',line)
-			if ret is not None:
-				count_timestamp += 1
-				idx_timestamp.append(x)
-				continue
-			count_str += 1
-			idx_str.append(x)
-		elif(type(X[row,x]) is bool):
-			count_bool += 1	
-			idx_bool.append(x)		
-		elif(type(X[row,x]) is int):
-			count_int += 1
-			idx_int.append(x)
-		elif(type(X[row,x]) is float):
-			count_float += 1
-			idx_float.append(x)
-	print 'count_str = ',count_str,', count_int = ',count_int,', count_bool = ',count_bool,', count_float = ',count_float,', count_timestamp = ',count_timestamp
-	print 'idx_timestamp = ',idx_timestamp
-
-
 def analyze_and_modify_nan_in_str_features(df):
 	# df.columns.str.lower()
 	print '\n\nremaining str cols = \n'
@@ -114,27 +68,6 @@ def encode_str_features_into_numerics(df, str_feats):
 
 		df[f] = df[f].replace('-1', -1, regex=True)
 	return df, str2int
-
-def encode_str_features(X, dict_str):
-	# prepare str feature->int mapping
-	dict_str2int = dict()
-	for idx_feat in dict_str:
-		if idx_feat not in dict_str2int:
-			dict_str2int[idx_feat] = dict()		
-
-		features = dict_str[idx_feat]
-		for x,f in enumerate(features):
-			if f not in dict_str2int[idx_feat]:
-				dict_str2int[idx_feat][f] = dict()
-			dict_str2int[idx_feat][f] = x
-
-	print 'dict_str2int = ',dict_str2int
-
-	# time-stamp features' index
-	timestamp = [155, ]
-
-	# condft str feature->int
-	_X = deepcopy(X)
 
 
 def delete_timestamp_cols(df):
@@ -283,7 +216,7 @@ def delete_str_cols_with_high_empty(df, _frac):
 	return df_cat 
 
 
-def perprocess_train_features(df, labels):
+def prune_train_features(df):
 	# Junk cols - Some feature engineering needed here
 	# df = df.ix[:, 520:660].fillna(-1)
 	# df = df.ix[:, :].fillna(-1)
@@ -343,17 +276,11 @@ def perprocess_train_features(df, labels):
 
 	# Check a boolean condition
 	# print (df.ix[:,'VAR_1933'] > 9998).any()
+	return df
 
+	
+def encode_features_for_training(df, labels):	
 	X = df.values.copy()
-	# print X
-	# impute missing values
-	# imp = Imputer(missing_values='NA', strategy='most_frequent', axis=0)
-	# imp.fit(X)
-	# X_imp = imp.transform(X)
-
-	# type of features
-	# feature_type(X)
-	# analyze_str_features(X)
 
     # np.random.shuffle(X)
 	X = X.astype(np.float32)
--- springleaf/scripts/main.py
+++ springleaf/scripts/main.py
@@ -6,13 +6,13 @@ import random
 import lasagne
 
 from svmClassifier import trySVM
-from featureProcessing import perprocess_train_features
+from featureProcessing import prune_train_features, encode_features_for_training
 
 # http://pandas.pydata.org/pandas-docs/stable/tutorials.html
 
 def load_train_data(path):
 	print("Loading Train Data...")
-	df = pd.read_csv(path, nrows=10000)
+	df = pd.read_csv(path, nrows=1000)
 	print("Loaded Train Data.")
 
 	labels = df.target
@@ -69,8 +69,9 @@ if __name__ == '__main__':
 
 	# Training data
 	df, labels = load_train_data(train_data_file)
-	X, y, encoder, scaler = perprocess_train_features(df,labels)
-
+	df_pruned = prune_train_features(df)
+	X, y, encoder, scaler = encode_features_for_training(df_pruned,labels)
+	
 	print('Number of classes:', len(encoder.classes_))
 	num_classes = len(encoder.classes_)
 	num_features = X.shape[1]
